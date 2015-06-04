20d2a3816e64541c175d991ed0805dfd54873865
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-06-04 18:04:03 +0900

    Remove empty line.

--- http.go
+++ http.go
@@ -69,10 +69,8 @@ func (s *Server) authHandler(w http.ResponseWriter, r *http.Request, _ denco.Par
 
 func (s *Server) indexHandler(w http.ResponseWriter, r *http.Request, _ denco.Params) {
 	if s.storage.token.hasToken() {
-
 		loadAce(w, "index", s.storage.Index())
 	} else {
-
 		loadAce(w, "before_auth", nil)
 	}
 }
