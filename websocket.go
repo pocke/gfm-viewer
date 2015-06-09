a53f5a609780e97cb9516de46e5c0ad64e05f13f
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-06-09 22:56:43 +0900

    mutex

--- websocket.go
+++ websocket.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"sync"
 
 	"golang.org/x/net/websocket"
 
@@ -18,6 +19,7 @@ func NewWSManager(ch <-chan string) WSManager {
 	w := &wsManager{
 		onUpdate: ch,
 		sessions: make(map[int]chan string),
+		mu:       &sync.RWMutex{},
 	}
 
 	go w.watch()
@@ -27,22 +29,23 @@ func NewWSManager(ch <-chan string) WSManager {
 
 type wsManager struct {
 	onUpdate <-chan string
-	// TODO: mutex
+
 	sessions map[int]chan string
+	mu       *sync.RWMutex
 }
 
 func (wsm *wsManager) ServeWS(w http.ResponseWriter, r *http.Request, _ denco.Params) {
 	websocket.Handler(func(ws *websocket.Conn) {
 		id := uniqID()
 		ch := make(chan string)
-		wsm.sessions[id] = ch
+		wsm.withLock(func() { wsm.sessions[id] = ch })
 		onClose := wsm.onClose(ws)
 		for {
 			select {
 			case path := <-ch:
 				ws.Write([]byte(path))
 			case <-onClose:
-				delete(wsm.sessions, id)
+				wsm.withLock(func() { delete(wsm.sessions, id) })
 				close(ch)
 				return
 			}
@@ -66,10 +69,18 @@ func (w *wsManager) onClose(ws *websocket.Conn) <-chan signal {
 
 func (w *wsManager) watch() {
 	for path := range w.onUpdate {
+		w.mu.RLock()
 		for _, ch := range w.sessions {
 			go func() { ch <- path }()
 		}
+		w.mu.RUnlock()
 	}
+}
+
+func (w *wsManager) withLock(f func()) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	f()
 }
 
 var uniqID = func() func() int {
