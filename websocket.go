16f0436c76e875b0e25e18c13b55043d1c73f3cc
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-06-09 21:54:21 +0900

    WebSocket Server

--- websocket.go
+++ websocket.go
@@ -2,69 +2,87 @@ package main
 
 import (
 	"net/http"
-	"sync"
+
+	"golang.org/x/net/websocket"
 
 	"github.com/naoina/denco"
-	"golang.org/x/net/websocket"
 )
-
-type WSManager struct {
-	sessions map[string][]chan signal
-	mu       *sync.RWMutex
-
-	receiveUpdate <-chan string
-}
 
 type signal struct{}
 
-func NewWSManager(ch <-chan string) *WSManager {
-	w := &WSManager{
-		sessions:      make(map[string][]chan signal),
-		mu:            &sync.RWMutex{},
-		receiveUpdate: ch,
+type WSManager interface {
+	ServeWS(w http.ResponseWriter, r *http.Request, _ denco.Params)
+}
+
+func NewWSManager(ch <-chan string) WSManager {
+	w := &wsManager{
+		onUpdate: ch,
+		sessions: make(map[int]chan string),
 	}
+
 	go w.watch()
 
 	return w
 }
 
-func (wsm *WSManager) ServeWS(w http.ResponseWriter, r *http.Request, p denco.Params) {
+type wsManager struct {
+	onUpdate <-chan string
+	// TODO: mutex
+	sessions map[int]chan string
+}
+
+func (wsm *wsManager) ServeWS(w http.ResponseWriter, r *http.Request, _ denco.Params) {
 	websocket.Handler(func(ws *websocket.Conn) {
-		ch := wsm.add(p.Get("path"))
-		<-ch
-		ws.Write([]byte("Update!"))
+		id := uniqID()
+		ch := make(chan string)
+		wsm.sessions[id] = ch
+		onClose := wsm.onClose(ws)
+		for {
+			select {
+			case path := <-ch:
+				ws.Write([]byte(path))
+			case <-onClose:
+				delete(wsm.sessions, id)
+				close(ch)
+				return
+			}
+		}
 	}).ServeHTTP(w, r)
 }
 
-func (w *WSManager) add(path string) <-chan signal {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-
-	s, ok := w.sessions[path]
-	if !ok {
-		s = make([]chan signal, 0, 1)
-	}
-
+func (w *wsManager) onClose(ws *websocket.Conn) <-chan signal {
 	ch := make(chan signal)
-	w.sessions[path] = append(s, ch)
+	go func() {
+		for {
+			_, err := ws.Read(make([]byte, 512))
+			if err != nil {
+				ch <- signal{}
+				break
+			}
+		}
+	}()
 	return ch
 }
 
-func (w *WSManager) watch() {
-	for {
-		path := <-w.receiveUpdate
-		Log("Update %s", path)
-
-		w.mu.Lock()
-		s, ok := w.sessions[path]
-		if !ok {
-			w.mu.Unlock()
-			continue
+func (w *wsManager) watch() {
+	for path := range w.onUpdate {
+		for _, ch := range w.sessions {
+			go func() { ch <- path }()
 		}
-		for _, v := range s {
-			v <- signal{}
-		}
-		delete(w.sessions, path)
-		w.mu.Unlock()
 	}
 }
+
+var uniqID = func() func() int {
+	ch := make(chan int)
+	go func() {
+		i := 0
+		for {
+			ch <- i
+			i++
+		}
+	}()
+
+	return func() int {
+		return <-ch
+	}
+}()
